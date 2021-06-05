254e1d6c630e2ff0c3768becd76d53cf6a1cd751
Paúl Ganán <paulisrael95@gmail.com>
2021-06-04 23:14:16 -0500

    locations view styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
-import React,{useContext} from 'react'
-import {ThemeContext} from './../context/ThemeContext'
+import React,{useContext} from 'react';
+import {ThemeContext} from './../context/ThemeContext';
+import {Link} from 'react-router-dom';
 export default function Header() {
     const {theme, setTheme} = useContext(ThemeContext) 
     const handleActive=()=>{
@@ -7,12 +8,14 @@ export default function Header() {
     }
     
     return (
-        <nav className={theme?'darkMode': 'witheMode'}>
+        <nav className={`hover-nav ${theme?'darkMode': 'witheMode'}` }>
+            <ul>
+                <li><Link to="/">Character</Link></li>
+                <li><Link to="/locations">Locations</Link></li>
+                <li><Link to="/locations">Episode</Link></li>
+            </ul>
             <div>
                 <button onClick={handleActive}>{theme?'Dark mode active': 'Active darkMode?'}</button>
-            </div>
-            <div>
-                more data
             </div>
         </nav>
         
--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,16 +1,49 @@
-import React, {useState,useEffect} from 'react'
-
+import React, {useState,useEffect, useContext} from 'react';
+import {ThemeContext} from './../context/ThemeContext';
 export default function Locations() {
-    const [locations, setLocations] = useState([])
+    const [locations, setLocations] = useState([]);
+    const {theme} = useContext(ThemeContext);
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/location')
         .then(response=>response.json())
-        .then(locations=> console.log(locations))
+        .then(locationsResult=> setLocations(locationsResult.results))
     }, [])
     
     return (
-        <div>
-            UwU
+        <div className="characters">
+            {/* <div className="simple-card">
+                <div className="simple-card-header">
+                    <h1>Name</h1>
+                </div>
+                <div className="simple-card-body">
+                    <div>dimention</div>
+                    <div>type</div>
+                </div>
+                <div className="simple-card-actions">
+                    <button>
+                        uwu
+                    </button>
+                </div>
+            </div> */}
+            {
+                locations.map(location=>(
+                    <div key={location.id} className={`character ${theme?"darkModeCard":""}`}>
+                        {/* <img src={character.image} alt="" /> */}
+                        <h3>{location.name}</h3>
+                        <div className="focus-content">
+                            <h1>{location.name}</h1>
+                            <div className="info">
+                                <p>Dimencion: {location.dimencion}</p>
+                                <p>Type: {location.type}</p>
+                                {/* <button onClick={()=>handleClick(character)}></button> */}
+                            </div>
+                                
+                            
+                        </div>
+                    </div>
+                    
+                ))
+            }
         </div>
     )
 }
